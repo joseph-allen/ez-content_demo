5d8d39cb26b09a70971f4d763bc26f974f06fa3c
Joe RN <joseph.allen.1994@hotmail.com>
2020-03-06 15:00:35 +0000

    Add niceish styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import EZContentDemo from './EZContentDemo/EZContentDemo';
 function App() {
   return (
     <div className="App">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       <header className="App-header">
         <EZContentDemo />
       </header>
--- src/EZContentDemo/EZContentDemo.js
+++ src/EZContentDemo/EZContentDemo.js
@@ -1,25 +1,51 @@
 import React from 'react';
 import './EZContentDemo.css';
+import { Card } from '@material-ui/core';
 
-function EZContentDemo() {
-    return (
-        <div className="EZContentDemo">
-            <p>Hi, Welcome to our site!</p>
+class EZContentDemo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.enableMessage = this.enableMessage.bind(this);
 
-            <p>Could be</p>
+        this.state = {
+            loaded: false,
+        };
 
-            <p><span className="ezcontent">Good Afternoon</span> <span className="ezcontent">Joe</span>, Welcome to our site!
+        this.timer = setTimeout(this.enableMessage, 2000);
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timer);
+    }
+
+    enableMessage() {
+        this.setState({ loaded: true });
+    }
+    render() {
+        let loadedClass = this.state.loaded ? 'ezcontent__loaded' : 'ezcontent__loading'
+        return (
+            <div >
+                <Card className={"ezcontent-card"}>
+                    <p>Hi, Welcome to our site!</p>
+                </Card>
+
+                <p>with EZContent could be...</p>
+
+                <Card className={"ezcontent-card"}>
+                    <p><span className={loadedClass}>{this.state.loaded ? 'Good afternoon' : '...'}</span> <span className={loadedClass}>{this.state.loaded ? 'John' : '...'}</span>, Welcome to our site!
                 </p>
-            <p>
-                We see it's a <span className="ezcontent">rainy day</span> in <span className="ezcontent">Manchester :(</span>
-            </p>
-            <p>We see you are on the move on your <span className="ezcontent">mobile</span>, so we've given you the mobile version of our sit</p>
-            <p>
-                Maybe you'd like to <span className="ezcontent">buy a coat?</span>
-            </p>
-            <p> You speak <span className="ezcontent">Japanese</span>? Awesome, would you like to see the <span className="ezcontent">Japanese</span> version of this site?</p>
-        </div >
-    );
+                    <p>
+                        We see it's a <span className={loadedClass}>{this.state.loaded ? 'sunny day' : '...'}</span> in <span className={loadedClass}>{this.state.loaded ? 'Manchester, UK' : '...'}</span>
+                    </p>
+                    <p>We see you are on the move on your <span className={loadedClass}>{this.state.loaded ? 'mobile' : '...'}</span>, so we've given you the <span className={loadedClass}>{this.state.loaded ? 'mobile' : '...'}</span> version of our site</p>
+                    <p>
+                        Maybe you'd like to <span className={loadedClass}>{this.state.loaded ? 'browse some coats?' : '...'}</span>
+                    </p>
+                    <p> You speak <span className={loadedClass}>{this.state.loaded ? 'Japanese' : '...'}</span>? Awesome, would you like to see the <span className="ezcontent">{this.state.loaded ? 'Japanese' : '...'}</span> version of this site?</p>
+                </Card>
+            </div >
+        );
+    }
 }
 
 export default EZContentDemo;
